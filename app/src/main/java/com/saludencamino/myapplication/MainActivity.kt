6603128f539b6bb795b0701737c5f48f5e0399f0
Nicolás Palacios Díaz <njpd57@gmail.com>
2022-05-17 13:47:30 -0400

    diseno

---
+++ app/src/main/java/com/saludencamino/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.saludencamino.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+    /** Called when the user touches the button */
+    fun login(view: View) {
+        println("LOGIN")
+val intent = Intent(this,homeActivity::class.java)
+        startActivity(intent)
+        // Do something in response to button click
+    }
+
+
+}
